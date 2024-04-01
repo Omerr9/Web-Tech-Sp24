49ec7a2fa28a715c0c7788bd7646090beb88f6fd
Omerr9 <omerfarooqbinatif1@gmail.com>
2024-04-01 13:38:10 +0500

    JS in progress 2

--- Mid-Term Lab Exam/script.js
+++ Mid-Term Lab Exam/script.js
@@ -1,16 +1,16 @@
-var logo = document.getElementById("logo");
-var images = document.querySelector(img);
+var logo = document.getElementById("img-name");
+var images = document.querySelectorAll('img');
 
 images.forEach(image => {
     // Add 'mouseover' event listener
     image.addEventListener('mouseover', () => {
       // Add the 'hovered' class on mouseover
-      logo.innerHTML = "Photo Hovered"
+      logo.innerHTML = "Photo Hovered";
     });
 
     // Add 'mouseout' event listener
     image.addEventListener('mouseout', () => {
       // Remove the 'hovered' class on mouseout
-      logo.innerHTML = ""
+      logo.innerHTML = "";
     });
-  });
+});
